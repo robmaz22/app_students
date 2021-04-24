dfadf2745a8b77495a80933a32c05c125c46ea5c
robmaz22 <czowieg@gmail.com>
2021-04-24 21:59:22 +0200

    dodanie funkcji wyświetlającej n najlepszych uczniów (test, ogólnie)

--- database/create_database.py
+++ database/create_database.py
@@ -17,3 +17,43 @@ def create_from_txt(path_to_txt):
 
     con.commit()
     con.close()
+
+def highest_score_test(n=3):
+    con = sqlite3.connect('pierwiastki_zycia.db')
+    cur = con.cursor()
+
+    cur.execute('''SELECT 
+    name, sname, school, test
+    FROM students
+    ORDER BY test DESC''')
+
+    results = cur.fetchall()
+
+    for idx in range(n):
+        print(idx+1, 'MIEJSCE:')
+        print(f'{results[idx][0]} {results[idx][1]} ({results[idx][2]})\n{results[idx][3]} punktów')
+        print('-'*15)
+
+    con.commit()
+    con.close()
+
+def highest_score_total(n=3):
+    con = sqlite3.connect('pierwiastki_zycia.db')
+    cur = con.cursor()
+
+    cur.execute('''SELECT 
+    name, sname, school, test, work
+    FROM students
+    ORDER BY (test + work) DESC''')
+
+    results = cur.fetchall()
+
+    for idx in range(n):
+        print(idx+1, 'MIEJSCE:')
+        total = int(results[idx][3]) + int(results[idx][4])
+
+        print(f'{results[idx][0]} {results[idx][1]} ({results[idx][2]})\n{total} punktów')
+        print('-'*15)
+
+    con.commit()
+    con.close()
--- pierwiastki_zycia.py
+++ pierwiastki_zycia.py
@@ -1,1 +1,14 @@
-#main app in console
+
+
+print('WITAJ W PROGRAMIE PIERWIASTKI ŻYCIA!')
+print('WYBIERZ OPCJĘ:\n1. Rozpocznij analizę\n2. Edytuj bazę danych\n3. Zakończ')
+option = input('>>> ')
+
+if int(option) == 1:
+    pass
+elif int(option) == 2:
+    pass
+elif int(option) == 3:
+    exit(0)
+else:
+    print('Niepoprawna wartość')
