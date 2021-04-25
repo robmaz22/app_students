de45a51bc754d87177b7e17e007a7b59c07d66b9
robmaz22 <czowieg@gmail.com>
2021-04-25 10:52:38 +0200

    zmiana modułów

--- pierwiastki_zycia_console.py
+++ pierwiastki_zycia_console.py
@@ -1,10 +1,11 @@
 import os
-from database import create_database
+from database.create_database import create_from_txt
+from database import database_analysis
 
 
 def make_analysis():
     if not os.path.exists('pierwiastki_zycia.db'):
-        create_database.create_from_txt('students.txt')
+        create_from_txt('students.txt')
 
     print('Ile miejsc?')
     n = int(input('>>> '))
@@ -14,10 +15,10 @@ def make_analysis():
     print('=' * 16)
     print()
     print('* NAJLEPSZY TEST:')
-    create_database.highest_score_test(n)
+    database_analysis.highest_score_test(n)
     print()
     print('* NAJLEPSZY WYNIK OGÓLNY:')
-    create_database.highest_score_total(n)
+    database_analysis.highest_score_total(n)
 
 
 print('WITAJ W PROGRAMIE PIERWIASTKI ŻYCIA!')
