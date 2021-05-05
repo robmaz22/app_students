099cc516b948739598ec6c525409e1e660a07914
robmaz22 <czowieg@gmail.com>
2021-05-05 23:35:38 +0200

    dodanie funkcji przypisywania punktów dla 5 zespołów

--- app_tkinter_GUI.py
+++ app_tkinter_GUI.py
@@ -4,6 +4,7 @@ from database import create_database
 from tkinter import messagebox
 from database import database_analysis
 
+
 class mainWindow:
 
     # Inicjalizacja
@@ -11,6 +12,7 @@ class mainWindow:
         self.master = master
         self.master.title('My App')
         self.master.geometry('440x170')
+        self.master.resizable(False, False)
 
         self.create_menu()
         self.welcome_screen()
@@ -37,7 +39,8 @@ class mainWindow:
         self.title_label.pack(pady=15, padx=20)
         self.create_button = tk.Button(self.welcome_frame, text='Utwórz nową bazę danych', command=self.create_new)
         self.create_button.pack(pady=5)
-        self.open_button = tk.Button(self.welcome_frame, text='Otwórz istniejącą bazę danych', command=lambda: self.start_analysis(True))
+        self.open_button = tk.Button(self.welcome_frame, text='Otwórz istniejącą bazę danych',
+                                     command=lambda: self.start_analysis(True))
         self.open_button.pack(pady=5)
 
     # wybór tworzenia nowej bazy
@@ -52,23 +55,25 @@ class mainWindow:
 
     # wyświetlenie wyników analizy
     def print_result(self):
+
+        self.master.geometry('600x500+200+200')
         self.analysis_frame.pack_forget()
 
         self.results_frame = tk.Frame(self.master, bg='white')
         self.results_frame.pack(fill='both', expand=1)
 
-        text_box = tk.Text(self.results_frame, height = 15, width = 52)
+        text_box = tk.Text(self.results_frame, height=15, width=52)
 
-        self.result = f'{"="*52}\n\t\t  UZYSKANE WYNIKI\n{"="*52}\n'
+        self.result = f'{"=" * 52}\n\t\t  UZYSKANE WYNIKI\n{"=" * 52}\n'
         self.result += '* NAJLEPSZY TEST:\n'
         self.result += database_analysis.highest_score_test(3)
-        self.result += '* NAJLEPSZY WYNIK OGÓLNY'
+        self.result += f'\n{"+"*52}\n'
+        self.result += '\n* NAJLEPSZY WYNIK OGÓLNY\n'
         self.result += database_analysis.highest_score_total(3)
 
         text_box.insert(tk.END, self.result)
         text_box.config(state='disabled')
         text_box.pack()
-
 
     # wybór po załadowaniu bazy
     def start_analysis(self, n):
@@ -77,10 +82,15 @@ class mainWindow:
         else:
             self.create_frame.pack_forget()
 
+        mbox = messagebox.askquestion('Wybór zespółów', 'Czy chcesz wylosować zespoły?')
+        if mbox == 'yes':
+            teams = teamsWindow()
+
         self.analysis_frame = tk.Frame(self.master, bg='white')
         self.analysis_frame.pack(fill='both', expand=1)
 
-        self.start_button = tk.Button(self.analysis_frame, text='Rozpocznij analizę', command=self.print_result).pack(pady=30)
+        self.start_button = tk.Button(self.analysis_frame, text='Rozpocznij analizę', command=self.print_result).pack(
+            pady=30)
         self.insert_button = tk.Button(self.analysis_frame, text='Edytuj bazę').pack(pady=10)
 
     # Otwieranie bazy danych z pliku txt z podanej lokalizacji
@@ -112,6 +122,42 @@ class mainWindow:
 
         self.save_button = tk.Button(self.edit_window, text='Zapisz').grid(column=0, row=7, padx=10, pady=10)
         self.delete_button = tk.Button(self.edit_window, text='Wyczyść dane').grid(column=1, row=7, padx=10, pady=10)
+
+
+class teamsWindow(tk.Toplevel):
+    def __init__(self):
+        tk.Toplevel.__init__(self)
+        self.geometry('150x150')
+        self.title('Wybór zespołów')
+        self.resizable(False, False)
+
+        # self.frame = tk.Frame(self, bg='white').pack(fill='both', expand=1)
+
+        self.entrys = [tk.Entry(self, width=5) for i in range(5)]
+
+        for n , entry in enumerate(self.entrys):
+            tk.Label(self, text=f'Zespół {n+1}').grid(padx=10, column=0, row=n)
+            entry.grid(padx=5, column=1, row=n)
+
+        accept_button = tk.Button(self, text='Zatwierdź', command=self.set_teams).grid(column=0, row=5, columnspan=2)
+
+    def set_teams(self):
+
+        points_dict = {}
+
+        for key, entry in enumerate(self.entrys):
+            points_dict[key] = int(entry.get())
+
+        create_database.set_teams(5)
+        create_database.set_team_points(points_dict)
+        self.destroy()
+
+
+
+
+
+
+
 
 
 # Uruchomienie programu
