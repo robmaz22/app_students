d166e05aa5a058dc2d7ec613483a7986c508843b
robmaz22 <czowieg@gmail.com>
2021-11-05 22:29:52 +0100

    New version

--- app_console.py
+++ app_console.py
@@ -1,69 +1,187 @@
 import os
-from database.create_database import *
-from database.database_analysis import *
+import sqlite3
+import csv
+from student_generator import generator
 
 
-def edit_students():
-    if not os.path.exists('pierwiastki_zycia.db'):
-        print('Baza danych jest pusta nie można edytować!')
+### database functions ##########
+def create_from_csv(path):
+    conn = sqlite3.connect('students.db')
+    conn.execute('''CREATE TABLE STUDENTS 
+    (id, sex, first_name, last_name, city, test, work)''')
+    cursor = conn.cursor()
+
+    with open(path, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        for idx, row in enumerate(reader):
+            row.insert(0, idx + 1)
+            row = tuple(row)
+            cursor.execute(f'''INSERT INTO STUDENTS 
+            (id, sex, first_name, last_name, city, test, work)
+            VALUES {row}''')
+
+    print('Database created successfully!')
+    conn.commit()
+    conn.close()
+
+
+def best_test(number):
+    conn = sqlite3.connect('students.db')
+    cursor = conn.cursor()
+
+    cursor.execute(F'''SELECT * FROM STUDENTS
+    ORDER BY cast(test as int) DESC
+    LIMIT {number}''')
+
+    for place, student in enumerate(cursor.fetchall()):
+        student_info = f'\t*{place + 1} place:\n'
+        student_info += f'\tFirst Name: {student[2]}\n'
+        student_info += f'\tLast Name: {student[3]}\n'
+        student_info += f'\tCity: {student[4]}\n'
+        student_info += f'\tTest points: {student[5]}\n'
+        print(student_info)
+
+    conn.close()
+
+
+def best_total(number):
+    conn = sqlite3.connect('students.db')
+    cursor = conn.cursor()
+
+    cursor.execute(F'''SELECT * FROM STUDENTS
+    ORDER BY cast(test as int) + cast(work as int) DESC
+    LIMIT {number}''')
+
+    for place, student in enumerate(cursor.fetchall()):
+        student_info = (f'\t*{place + 1} place:\n'
+                        f'\tFirst Name: {student[2]}\n'
+                        f'\tLast Name: {student[3]}\n'
+                        f'\tCity: {student[4]}\n'
+                        f'\tTest points: {student[5]}\n'
+                        f'\tWork points: {student[6]}\n'
+                        f'\tTOTAL: {int(student[5]) + int(student[6])}\n')
+        print(student_info)
+
+    conn.close()
+
+
+def advanced():
+    conn = sqlite3.connect('students.db')
+    cursor = conn.cursor()
+    # number of students
+    cursor.execute('''SELECT COUNT(id) FROM STUDENTS''')
+    total = cursor.fetchone()[0]
+    print(f'Total students number: {total} where,')
+    cursor.execute('''SELECT COUNT(id) FROM STUDENTS WHERE sex = "f"''')
+    women = cursor.fetchone()[0]
+    print(f'* Women: {women} ({(women / total) * 100:.1f}%)')
+    cursor.execute('''SELECT COUNT(id) FROM STUDENTS WHERE sex = "m"''')
+    men = cursor.fetchone()[0]
+    print(f'* Men: {men} ({(men / total) * 100:.1f}%)')
+    print()
+    # average test
+    cursor.execute('''SELECT AVG(test) FROM STUDENTS''')
+    print(f'Average test result: {cursor.fetchone()[0]:.1f} where,')
+    cursor.execute('''SELECT AVG(test) FROM STUDENTS WHERE sex = "f"''')
+    print(f'* Women: {cursor.fetchone()[0]:.1f}')
+    cursor.execute('''SELECT AVG(test) FROM STUDENTS WHERE sex = "m"''')
+    print(f'* Men: {cursor.fetchone()[0]:.1f}')
+    print()
+    # average work
+    cursor.execute('''SELECT AVG(work) FROM STUDENTS''')
+    print(f'Average work result: {cursor.fetchone()[0]:.1f} where,')
+    cursor.execute('''SELECT AVG(work) FROM STUDENTS WHERE sex = "f"''')
+    print(f'* Women: {cursor.fetchone()[0]:.1f}')
+    cursor.execute('''SELECT AVG(work) FROM STUDENTS WHERE sex = "m"''')
+    print(f'* Men: {cursor.fetchone()[0]:.1f}')
+
+    conn.close()
+
+
+def edit():
+    flag = False
+    if input('Do you want to create new database?[y/n]\n>>>') == 'y':
+        os.remove('students.db')
+        path = input('File path to new csv file: ')
+        create_from_csv(path)
         return
 
-    student_list = base_content()
-    print('LISTA UCZNIÓW:')
+    conn = sqlite3.connect('students.db')
+    cursor = conn.cursor()
+    cursor.execute('''SELECT * FROM STUDENTS''')
+    students_list = [generator.Student(info) for info in cursor.fetchall()]
 
-    for id, info in enumerate(student_list):
-        print(f'{id + 1}. {info[0]} {info[1]}')
+    if input('Do you want to show all students?[y/n]\n>>>') == 'y':
+        for nr, student in enumerate(students_list):
+            print(f'*STUDENT {nr + 1}')
+            print(student)
 
     while True:
-        print('Wybierz indeks, który chcesz edytować (Aby zatrzymać wpisz stop):')
-        idx = input('>>> ')
-
-        if idx == 'stop':
+        print('Student index to edit (Type stop if you finished)')
+        try:
+            index = int(input('>>>')) - 1
+        except ValueError:
             break
 
-        edit_record(int(idx))
+        print('Chosen student:')
+        print(students_list[index])
+        print('Type new information (use commas):')
+        info = input('>>>')
+        students_list[index].edit(info)
+        flag = True
+
+    if flag:
+        generator.save_database(db_file='students.db', students_list=students_list)
+        if input('Do you want to create csv file too?[y/n]') == 'y':
+            generator.save_to_csv(output_file='students.csv', students_list=students_list)
+
+    conn.close()
 
 
-def make_analysis():
+################################
+
+
+def run():
     if not os.path.exists('students.db'):
-        create_from_txt('students.txt')
-        print('Podaj liczbę zespołów:')
-        groups = int(input('>>> '))
-        set_teams(groups)
+        print('No database file. Do you want to create it?[y/n]')
+        answer = input('>>>')
+        if answer.lower() == 'y':
+            path = input('File path: ')
+            create_from_csv(path)
 
-        points = {}
+    print('How many places to show?')
+    how_many = int(input('>>>'))
 
-        for group in range(groups):
-            print(f'Podaj ilość punktów dla zespołu {group + 1}:')
-            pts = int(input('>>>'))
-            points[group + 1] = pts
+    result_dict = {'TEST WINNERS:': best_test,
+                   'TOTAL WINNERS:': best_total}
 
-        set_team_points(points)
+    for key, func in result_dict.items():
+        print(key)
+        func(how_many)
 
-    print('[INFO] ROZPOCZĘCIE ANALIZY...')
-    print('Ile miejsc wyświetlić?')
-    n = int(input('>>> '))
-
-    print('=' * 16)
-    print('UZYSKANE WYNIKI:')
-    print('=' * 16)
-    print()
-    print('* NAJLEPSZY TEST:')
-    highest_score_test(n)
-    print()
-    print('* NAJLEPSZY WYNIK OGÓLNY:')
-    highest_score_total(n)
+    print('Do you want to start more advanced analysis?[y/n]')
+    answer = input('>>>')
+    if answer.lower() == 'y':
+        advanced()
 
 
-print('WITAJ W PROGRAMIE DO ANALIZY DANYCH!')
-print('WYBIERZ OPCJĘ:\n1. Rozpocznij analizę\n2. Edytuj bazę danych\n3. Zakończ')
-option = input('>>> ')
+def main():
+    print('What do you want to do?')
+    print('1. Start analysis')
+    print('2. Edit students')
+    print('3. Exit')
+    answer = input('>>>')
 
-if int(option) == 1:
-    make_analysis()
-elif int(option) == 2:
-    edit_students()
-elif int(option) == 3:
-    exit(0)
-else:
-    print('Niepoprawna wartość')
+    if answer == '1':
+        run()
+    elif answer == '2':
+        edit()
+        run()
+    elif answer == '3':
+        exit()
+    else:
+        print('Wrong value')
+
+
+if __name__ == '__main__':
+    main()
---
+++ student_generator/generator.py
@@ -0,0 +1,120 @@
+import os
+import random
+import csv
+import argparse as ap
+import sqlite3
+
+
+class Student:
+    info = None
+
+    @classmethod
+    def load_info(cls, language):
+        info_files = os.path.join('personal_information', language)
+
+        cls.info = {}
+
+        for filename in os.listdir(info_files):
+            full_path = os.path.join(info_files, filename)
+            with open(full_path, 'r') as file:
+                content = [word.rstrip() for word in file.readlines()]
+                basename = filename.split('.')[0]
+                cls.info[basename] = content
+
+    def __init__(self, info):
+        if info:
+            self.sex = info[1]
+            self.name = info[2]
+            self.last_name = info[3]
+            self.city = info[4]
+            self.test_points = info[5]
+            self.work_points = info[6]
+        else:
+            self.sex = random.choice(['m', 'f'])
+
+            if self.sex == 'm':
+                self.name = random.choice(self.info['male_names'])
+            else:
+                self.name = random.choice(self.info['female_names'])
+
+            self.last_name = random.choice(self.info['last_names'])
+            self.city = random.choice(self.info['cities'])
+            self.test_points = random.randint(1, 50)
+            self.work_points = random.randint(1, 50)
+
+    def __str__(self):
+        self._student_info = (f'Sex: {self.sex}\n'
+                              f'First Name: {self.name}\n'
+                              f'Last Name: {self.last_name}\n'
+                              f'City: {self.city}\n'
+                              f'Test Points: {self.test_points}\n'
+                              f'Work Points: {self.work_points}\n')
+        return self._student_info
+
+    def edit(self, new_info):
+        new_info_split = new_info.split(',')
+        self.sex = new_info_split[0]
+        self.name = new_info_split[1]
+        self.last_name = new_info_split[2]
+        self.city = new_info_split[3]
+        self.test_points = new_info_split[4]
+        self.work_points = new_info_split[5]
+        
+    def save_mode(self):
+        return self.sex, self.name, self.last_name, self.city, self.test_points, self.work_points
+
+
+def get_args():
+    parser = ap.ArgumentParser(description='Script to generate n student with random personal  information')
+    parser.add_argument('-n', '--number', type=int, required=True, help='Number of students to generate')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Show generated students')
+    parser.add_argument('-o', '--output', required=False, default='../students.csv', help='Output path for csv file')
+    parser.add_argument('-l', '--language', required=False, default='polish', help='Personal info language')
+
+    return parser.parse_args()
+
+
+def save_database(db_file, students_list):
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+    cursor.execute('DROP TABLE STUDENTS')
+    conn.execute('''CREATE TABLE STUDENTS 
+    (id, sex, first_name, last_name, city, test, work)''')
+
+    for idx, student in enumerate(students_list):
+        info = list(student.save_mode())
+        info.insert(0, idx+1)
+        cursor.execute(f'''INSERT INTO STUDENTS 
+        (id, sex, first_name, last_name, city, test, work)
+        VALUES {info}''')
+
+    print('Database updated successfully!')
+    conn.commit()
+    conn.close()
+
+
+def save_to_csv(output_file, students_list):
+    with open(output_file, 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        for student in students_list:
+            writer.writerow(student.save_mode())
+
+    print(f'File {os.path.basename(output_file)} with {output_file} students saved')
+
+
+def main():
+    args = get_args()
+    Student.load_info(args.language)
+
+    students = [Student(info=False) for _ in range(args.number)]
+
+    if args.verbose:
+        for i, student in enumerate(students):
+            print(f'STUDENT {i + 1}')
+            print(student)
+
+    save_to_csv(args.output, students)
+
+
+if __name__ == '__main__':
+    main()
